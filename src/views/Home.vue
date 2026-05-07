e81f06742cd2136de05149c6010bfe6d21a5c9e5
minaylos-dev <minaylos.dev@gmail.com>
2026-05-07 17:50:42 +0300

    init commit, add vue, vue-router, tailwind, fonts, pages

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen">
+        <h1 class="font-tinkoff text-white text-8xl text-center">Выбери свой сценарий<br/>будущего</h1>        
+    </div>
+    <div class="absolute bottom-10 left-1/2 -translate-x-1/2">
+        <RouterLink to="/quiz">
+             <p class="font-tinkoff text-white text-4xl text-center">Покрути шар чтобы <br/>начать</p>
+        </RouterLink>
+    </div>
+</template> 
---
+++ src/views/Result.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col grow items-end justify-end min-h-screen pb-48">
+        <div class="flex w-full justify-center">
+            <div class="flex items-start justify-center w-2/3 gap-10">
+                <h1 class="font-tinkoff text-white text-8xl">Название<br /> планеты</h1>
+                <p class="font-tinkoff text-white text-2xl">
+                    Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Eaque quidem cupiditate nihil, aperiam quaerat quibusdam! Qui assumenda temporibus
+                    magni modi veritatis Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis quibusdam
+                    esse porro vitae veniam voluptates vel ipsum iusto, at ex modi voluptas labore sapiente quisquam
+                    illo eos fuga quod. Nemo!
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="absolute bottom-10 left-1/2 -translate-x-1/2">
+        <RouterLink to="/">
+            <p class="font-tinkoff text-white text-4xl text-center">Результат</p>
+        </RouterLink>
+    </div>
+</template>
