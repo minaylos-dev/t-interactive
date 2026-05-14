e7600f8322026594ed9fa9bc7d1e36d8264f0df0
minaylos-dev <minaylos.dev@gmail.com>
2026-05-15 01:32:13 +0300

    enable fullscreen and disable devtools

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    fullscreen: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      devTools: false,
     },
   });
 
@@ -24,8 +24,7 @@ const createWindow = () => {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // DevTools are disabled by default for security.
 };
 
 // This method will be called when Electron has finished
